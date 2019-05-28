0a357e7daf5664331067143735e09d910b61557a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-28 18:22:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
-  validates :category, inclusion in: 
+  validates :category, inclusion {in: 'fiction' 'non-fiction'}
 end
